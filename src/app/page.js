e9fae8709f7857eff613b352878fa2305dea664c
zzeongho <junghoo022@gmail.com>
2023-08-11 17:28:51 +0900

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      Hello, Next Js!
+      <br></br>
+      <img src="/dogeCoin.png"></img>
+    </>
+  );
+}
